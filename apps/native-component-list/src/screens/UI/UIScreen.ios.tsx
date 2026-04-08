1bccecd30f18ee2954c36a7e52c37bff26a45b6f
Kudo Chien <kudo@expo.dev>
2026-04-09 00:07:15 +0800

    [ui] add LazyHStack/LazyVStack

--- apps/native-component-list/src/screens/UI/UIScreen.ios.tsx
+++ apps/native-component-list/src/screens/UI/UIScreen.ios.tsx
@@ -211,6 +211,14 @@ export const UIScreens = [
     },
   },
   {
+    name: 'LazyVStack / LazyHStack',
+    route: 'ui/lazy-stack',
+    options: {},
+    getComponent() {
+      return optionalRequire(() => require('./LazyStackScreen'));
+    },
+  },
+  {
     name: 'ScrollView component',
     route: 'ui/scrollview',
     options: {},
